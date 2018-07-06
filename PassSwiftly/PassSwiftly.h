a91eeca207c948ff1d54c1e9e1f391c4057de3d2
Isaac Halvorson <hello@hisaac.net>
2018-07-06 08:54:32 -0500

    Initial Commit

---
+++ PassSwiftly/PassSwiftly.h
@@ -0,0 +1,13 @@
+// Created by Isaac Halvorson on 7/6/18
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PassSwiftly.
+FOUNDATION_EXPORT double PassSwiftlyVersionNumber;
+
+//! Project version string for PassSwiftly.
+FOUNDATION_EXPORT const unsigned char PassSwiftlyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PassSwiftly/PublicHeader.h>
+
+
